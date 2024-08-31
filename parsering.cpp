9c8ec2e7d8477e5418cded530304efcbc2fae2cf
Demedhat29 <mohamed2012153@miuegypt.edu.eg>
2024-08-31 14:28:08 +0300

    Create parsering.cpp

---
+++ parsering.cpp
@@ -0,0 +1,32 @@
+// parsering.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+#include <iostream>
+#include <exception>
+#include <cctype>
+#include <string>
+#include <fstream>
+#include "Parser.h"
+
+int main()
+{
+	Parser::parseFromFile("client.txt");
+	Parser::parseFromFile("employee.txt");
+	Parser::parseFromFile("admin.txt");
+
+	return 0;
+   
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
